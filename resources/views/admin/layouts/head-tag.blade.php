709345a14b0d4f9e9db68d0d1250a886585d95c6
ashkanrabiee <ashkanrabiee52@gmail.com>
2025-04-27 06:45:30 -0700

    Initial commit

---
+++ resources/views/admin/layouts/head-tag.blade.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="{{ asset('admin-asset/css/bootstrap/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('admin-asset/fontawesome/css/all.min.css') }}">
+<link rel="stylesheet" href="{{ asset('admin-asset/css/animate.min.css') }}">
+<link rel="stylesheet" href="{{ asset('admin-asset/css/grid.css') }}">
+<link rel="stylesheet" href="{{ asset('admin-asset/select2/css/select2.min.css') }}">
+<link rel="stylesheet" href="{{ asset('admin-asset/sweetalert/sweetalert2.css') }}">
+<link rel="stylesheet" href="{{ asset('admin-asset/css/style.css') }}">
+
---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,158 @@
+<aside id="sidebar" class="sidebar">
+    <section class="sidebar-container">
+        <section class="sidebar-wrapper">
+
+            <section class="sidebar-part-title">داشبورد</section>
+            <a href="#" class="sidebar-link">
+                <i class="fas fa-home"></i>
+                <span>خانه</span>
+            </a>
+
+           
+         
+            <section class="sidebar-group-link">
+                <section class="sidebar-dropdown-toggle">
+                    <i class="fas fa-chart-bar icon"></i>
+                    <span>مدیریت کاربران</span>
+                    <i class="fas fa-angle-left angle"></i>
+                </section>
+                <section class="sidebar-dropdown">
+                    <a href="#">لیست کاربران</a>
+                    <a href="#">تایید/بلاک/حذف کاربر</a>
+                    <a href="#">مشاهده پروفایل</a>
+                    <a href="#">گزارش‌های ارسالی علیه کاربران</a>
+                </section>
+            </section>
+
+
+            
+         
+            <section class="sidebar-group-link">
+                <section class="sidebar-dropdown-toggle">
+                    <i class="fas fa-chart-bar icon"></i>
+                    <span>مدیریت آگهی‌ها</span>
+                    <i class="fas fa-angle-left angle"></i>
+                </section>
+                <section class="sidebar-dropdown">
+                    <a href="#">لیست آگهی‌ها</a>
+                    <a href="#">تایید/رد آگهی</a>
+                    <a href="#">ویرایش/حذف آگهی</a>
+                    <a href="#">آگهی‌های گزارش شده</a>
+                    <a href="#">دسته‌بندی آگهی‌ها</a>
+                </section>
+            </section>
+
+
+
+
+            <section class="sidebar-group-link">
+                <section class="sidebar-dropdown-toggle">
+                    <i class="fas fa-users icon"></i>
+                    <span>مدیریت دسته‌بندی‌ها</span>
+                    <i class="fas fa-angle-left angle"></i>
+                </section>
+                <section class="sidebar-dropdown">
+                    <a href="#">اضافه کردن دسته‌بندی جدید</a>
+                    <a href="#">ویرایش/حذف دسته‌بندی‌ها</a>
+                    <a href="#">دسته‌بندی‌های والد/فرزند</a>
+                </section>
+            </section>
+
+
+            <section class="sidebar-group-link">
+                <section class="sidebar-dropdown-toggle">
+                    <i class="fas fa-users icon"></i>
+                    <span>مدیریت پرداخت‌ها و پلن‌ها</span>
+                    <i class="fas fa-angle-left angle"></i>
+                </section>
+                <section class="sidebar-dropdown">
+                    <a href="#">مشاهده پرداخت‌ها</a>
+                    <a href="#">مدیریت پلن‌های ویژه</a>
+                    <a href="#">تایید یا بررسی مشکلات پرداخت</a>
+                </section>
+            </section>
+
+
+            <section class="sidebar-group-link">
+                <section class="sidebar-dropdown-toggle">
+                    <i class="fas fa-cogs icon"></i>
+                    <span>مدیریت گزارشات</span>
+                    <i class="fas fa-angle-left angle"></i>
+                </section>
+                <section class="sidebar-dropdown">
+                    <a href="#">آگهی‌های گزارش شده</a>
+                    <a href="#">کاربران گزارش شده</a>
+                    <a href="#">اقدامات لازم (حذف، هشدار، بلاک)</a>
+                </section>
+            </section>
+
+            <section class="sidebar-group-link">
+                <section class="sidebar-dropdown-toggle">
+                    <i class="fas fa-cogs icon"></i>
+                    <span>مدیریت پیام‌ها</span>
+                    <i class="fas fa-angle-left angle"></i>
+                </section>
+                <section class="sidebar-dropdown">
+                    <a href="#">مانیتور چت</a>
+                    <a href="#">حذف پیام‌های خلاف قوانین</a>
+                </section>
+            </section>
+
+
+            <section class="sidebar-group-link">
+                <section class="sidebar-dropdown-toggle">
+                    <i class="fas fa-cogs icon"></i>
+                    <span>تنظیمات سایت</span>
+                    <i class="fas fa-angle-left angle"></i>
+                </section>
+                <section class="sidebar-dropdown">
+                    <a href="#">تنظیمات تماس با ما</a>
+                    <a href="#">سیاست‌ها و قوانین</a>
+                    <a href="#">پیام‌های ایمیل/پیامک</a>
+                    <a href="#">تنظیمات ظاهر سایت</a>
+                </section>
+            </section>
+
+            <section class="sidebar-group-link">
+                <section class="sidebar-dropdown-toggle">
+                    <i class="fas fa-cogs icon"></i>
+                    <span>مدیریت تبلیغات</span>
+                    <i class="fas fa-angle-left angle"></i>
+                </section>
+                <section class="sidebar-dropdown">
+                    <a href="#">اضافه کردن بنر تبلیغاتی</a>
+                    <a href="#">مدیریت جایگاه تبلیغات</a>
+                </section>
+            </section>
+
+
+            <section class="sidebar-group-link">
+                <section class="sidebar-dropdown-toggle">
+                    <i class="fas fa-cogs icon"></i>
+                    <span>مدیریت نقش‌ها و دسترسی‌ها</span>
+                    <i class="fas fa-angle-left angle"></i>
+                </section>
+                <section class="sidebar-dropdown">
+                    <a href="#">ایجاد نقش‌های مختلف</a>
+                    <a href="#">تعیین سطح دسترسی برای هر نقش</a>
+                </section>
+            </section>
+
+            <section class="sidebar-group-link">
+                <section class="sidebar-dropdown-toggle">
+                    <i class="fas fa-cogs icon"></i>
+                    <span>گزارش‌های آماری</span>
+                    <i class="fas fa-angle-left angle"></i>
+                </section>
+                <section class="sidebar-dropdown">
+                    <a href="#">آمار بازدید سایت</a>
+                    <a href="#">آمار ثبت آگهی</a>
+                    <a href="#">بیشترین دسته‌بندی‌های فعال</a>
+                </section>
+            </section>
+
+
+
+        </section>
+    </section>
+</aside>
